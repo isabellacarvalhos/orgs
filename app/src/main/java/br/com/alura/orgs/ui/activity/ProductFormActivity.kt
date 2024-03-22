75cf8dd3cf67c2e11a3fc761866b1f3654cf84df
isabellacarvalho <isabella.carvalho@g.globo>
2024-03-22 19:47:41 -0300

    refactoring

--- app/src/main/java/br/com/alura/orgs/ui/activity/ProductFormActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/ProductFormActivity.kt
@@ -1,7 +1,6 @@
 package br.com.alura.orgs.ui.activity
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
@@ -12,30 +11,33 @@ import br.com.alura.orgs.model.Products
 class ProductFormActivity : AppCompatActivity(R.layout.activity_product_form) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        configureSaveButton()
+    }
 
-
+    private fun configureSaveButton() {
         val button = findViewById<Button>(R.id.button)
+        val dao = ProductsDao()
         button.setOnClickListener {
-            val name = findViewById<EditText>(R.id.name)
-            val nameProduct = name.text.toString()
-
-            val description = findViewById<EditText>(R.id.description)
-            val descriptionProduct = description.text.toString()
-
-            val price = findViewById<EditText>(R.id.price)
-            val priceProduct = price.text.toString()
-
-            val createdProduct = Products(
-                name = nameProduct,
-                description = descriptionProduct,
-                price = priceProduct
-            )
-            Log.d("Created Product", "createdProduct: $createdProduct")
-            val dao = ProductsDao()
+            val createdProduct = createdProduct()
             dao.add(createdProduct)
-            Log.d("Created Product", "createdProduct: ${dao.searchAll()}")
             finish()
         }
     }
 
+    private fun createdProduct(): Products {
+        val name = findViewById<EditText>(R.id.name)
+        val nameProduct = name.text.toString()
+
+        val description = findViewById<EditText>(R.id.description)
+        val descriptionProduct = description.text.toString()
+
+        val price = findViewById<EditText>(R.id.price)
+        val priceProduct = price.text.toString()
+
+        return Products(
+            name = nameProduct,
+            description = descriptionProduct,
+            price = priceProduct
+        )
+    }
 }

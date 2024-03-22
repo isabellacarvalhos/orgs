887a009117614641b12e3b678a6983b8ec19166e
isabellacarvalho <isabella.carvalho@g.globo>
2024-03-22 18:43:52 -0300

    created floating action button

--- app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
@@ -1,12 +1,15 @@
 package br.com.alura.orgs.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import br.com.alura.orgs.R
 import br.com.alura.orgs.model.Products
 import br.com.alura.orgs.ui.recyclerview.adapter.ProductsListAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
@@ -26,5 +29,11 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             )
         ))
         recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val actionButton = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        actionButton.setOnClickListener{
+            val intent = Intent(this, ProductFormActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

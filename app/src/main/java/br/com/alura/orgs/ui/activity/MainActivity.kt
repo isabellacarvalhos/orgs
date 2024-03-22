4ff21b39cb6ba03ab3d71b7fafc4c72d3edcac43
isabellacarvalho <isabella.carvalho@g.globo>
2024-03-22 17:02:46 -0300

    introduction to android X AppCompatActivity

--- app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/MainActivity.kt
@@ -8,12 +8,10 @@ import br.com.alura.orgs.R
 import br.com.alura.orgs.model.Products
 import br.com.alura.orgs.ui.recyclerview.adapter.ProductsListAdapter
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.adapter = ProductsListAdapter(this, products = listOf(
             Products(
--- app/src/main/java/br/com/alura/orgs/ui/activity/ProductFormActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/ProductFormActivity.kt
@@ -1,21 +1,7 @@
 package br.com.alura.orgs.ui.activity
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import br.com.alura.orgs.R
 
-class ProductFormActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_product_form)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
+class ProductFormActivity : AppCompatActivity(R.layout.activity_product_form) {
 }

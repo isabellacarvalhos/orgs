7b7c630b800f883ba42dd873bc39f335f05093c9
isabellacarvalho <isabella.carvalho@g.globo>
2024-04-23 18:43:26 -0300

    date added to layout

--- app/src/main/java/br/com/alura/orgs/ui/activity/ProductsListActivity.kt
+++ app/src/main/java/br/com/alura/orgs/ui/activity/ProductsListActivity.kt
@@ -2,12 +2,17 @@ package br.com.alura.orgs.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import br.com.alura.orgs.R
 import br.com.alura.orgs.dao.ProductsDao
 import br.com.alura.orgs.databinding.ActivityProductsListBinding
 import br.com.alura.orgs.ui.recyclerview.adapter.ProductsListAdapter
+import com.google.android.material.datepicker.MaterialDatePicker
+import java.time.Instant
+import java.time.ZoneId
+import java.time.ZoneOffset
 
 class ProductsListActivity : AppCompatActivity(R.layout.activity_products_list) {
 
@@ -22,6 +27,21 @@ class ProductsListActivity : AppCompatActivity(R.layout.activity_products_list) 
         setContentView(binding.root)
         configureRecyclerView()
         configureActionButton()
+
+
+        val dateSelector = MaterialDatePicker.Builder.datePicker().build()
+        dateSelector.show(supportFragmentManager, "MATERIAL_DATE_PICKER")
+
+        dateSelector.addOnPositiveButtonClickListener { dataInMiliseconds ->
+            Log.i("MaterialDatePicker", "data em milisegundos: $dataInMiliseconds")
+
+            val date = Instant.ofEpochMilli(dataInMiliseconds)
+                .atZone(ZoneId.of("America/Sao_Paulo"))
+                .withZoneSameInstant(ZoneId.ofOffset("UTC", ZoneOffset.UTC))
+                .toLocalDate()
+            Log.i("MaterialDatePicker", "data com LocalDate: $date")
+
+        }
     }
 
     override fun onResume() {
